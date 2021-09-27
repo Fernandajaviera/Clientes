928c2078a54d9117164ef6cc2ee08ec7844a112c
fernanda <fernandajaviera64@gmail.com>
2021-09-26 23:02:13 -0300

    .

--- clientes.sql
+++ clientes.sql
@@ -7,57 +7,60 @@ psql < unidad2.sql
 
 Ejercicio 2:
 
-a)Primero vamos a revisar si esta el registro del usuario01
+Primero vamos a revisar si esta el registro del usuario01
 
 SELECT id FROM cliente WHERE nombre = 'usuario01';
 
-b)después vamos a buscar si ha realizado las compras
+-
+BEGIN TRANSACTION;
 
+INSERT INTO compra(cliente_id, fecha) VALUES(1, now()) RETURNING id;
+INSERT INTO detalle_compra(producto_id, compra_id) VALUES(9, ID_ANTERIOR);
+UPDATE producto SET stock = stock - 5 WHERE id = 9;
 
-SELECT id, fecha FROM compra WHERE cliente_id = 1;
+END TRANSACTION;
 
- id |   fecha    
-----+------------
- 26 | 2020-01-31
- 31 | 2020-03-29
+SELECT stock FROM producto WHERE id = 9;
 
-SELECT * FROM producto WHERE descripcion = 'producto9';
-
- id | descripcion | stock | precio 
-----+-------------+-------+--------
-  9 | producto9   |     4 |   4219
-
-SELECT * FROM detalle_compra WHERE id = '1';
-
- id | producto_id | compra_id | cantidad 
-----+-------------+-----------+----------
-  1 |           2 |         2 |        9
-(1 row)
+El stock es -1, se quedo sin stock.
 
 Ejercicio 3:
 
-El primer paso es ver el stock de los productos:
 
-SELECT * FROM producto WHERE id in (1,2,8);
+BEGIN TRANSACTION;
 
- id | descripcion | stock | precio 
-----+-------------+-------+--------
-  1 | producto1   |     6 |   9107
-  2 | producto2   |     5 |   1760
-  8 | producto8   |     0 |   8923
-(3 rows)
-El producto 8 se queda sin stock.
+INSERT INTO compra(cliente_id, fecha) VALUES(2, now()) RETURNING id;
+INSERT INTO detalle_compra(producto_id, compra_id) VALUES(1, ID_ANTERIOR);
+INSERT INTO compra(cliente_id, fecha) VALUES(2, now()) RETURNING id;
+INSERT INTO detalle_compra(producto_id, compra_id) VALUES(2, ID_ANTERIOR);
+INSERT INTO compra(cliente_id, fecha) VALUES(2, now()) RETURNING id;
+INSERT INTO detalle_compra(producto_id, compra_id) VALUES(8, ID_ANTERIOR);
+UPDATE producto SET stock = stock - 3 WHERE id = 1;
+UPDATE producto SET stock = stock - 3 WHERE id = 2;
+UPDATE producto SET stock = stock - 3 WHERE id = 8;
+
+END TRANSACTION;
+
+SELECT stock FROM producto WHERE id = 1;
+SELECT stock FROM producto WHERE id = 2;
+SELECT stock FROM producto WHERE id = 8;
+
+El stock final de los productos fue:
+
+producto 1 se quedo con 3 de stock
+producto 2 se quedo con 2 de stock
+producto 8 no tenia stock, y ahora tiene de stock -3
 
 Ejercicio 4:
 Para desactivar AUTOCOMMIT
 
-a) \set AUTOCOMMIT off
+\set AUTOCOMMIT off
 
-b) Para insertar un nuevo cliente utilizaremos:
+Para insertar un nuevo cliente utilizaremos:
 
 INSERT INTO cliente (nombre, email) VALUES ('amelia', 'gatitos@123.cl');
 
-c) Comprobación del nuevo cliente: 
+Comprobación del nuevo cliente: 
 
 SELECT * FROM cliente WHERE nombre='amelia';
  id | nombre |     email      
@@ -65,16 +68,16 @@ SELECT * FROM cliente WHERE nombre='amelia';
  13 | amelia | gatitos@123.cl
 (1 row)
 
-d) ROLLBACK; 
+ROLLBACK; 
 (Para hacer un ROLLBACK la mejor opción es ir dejando puntos de guardado (savepoint) para regresar donde se desea)
 
-e) Verificación de la restauración:
+Verificación de la restauración:
 
 SELECT * FROM cliente WHERE nombre='amelia';
  id | nombre | email 
 ----+--------+-------
 (0 rows)
 
-f) Activación AUTOCMMIT:
+Activación AUTOCMMIT:
 
-\set AUTOCOMMIT off 
+\set AUTOCOMMIT on
